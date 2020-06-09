a68b0d32dbeade18fa6af85b62cb0cbac8da2e6e
cz <949710989@qq.com>
2020-06-09 21:12:34 +0800

    day2
    Signed-off-by: cz <949710989@qq.com>

---
+++ day2/src/tests/Test.java
@@ -0,0 +1,13 @@
+package tests;
+
+import interfacesimpl.PrinterIfIpml;
+
+/**
+ * @author cz
+ */
+public class Test {
+    public static void main(String[] args) {
+        new PrinterIfIpml().print();
+    }
+}
+
